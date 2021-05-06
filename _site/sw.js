5b40cff02d22e6ebe4970bd69e76eb1d80c1432e
dylan-losey <losey@vt.edu>
2021-05-06 00:52:55 -0400

    updates

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202011261059",
+        "/css/pixyll.css?202105060052",
         "/"
       ]);
     })
